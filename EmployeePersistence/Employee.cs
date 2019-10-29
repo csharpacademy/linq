a216e1bfe868125ffe83003764bdf1317c98339e
Piotr Myszor <PMyszor@rockwellautomation.com>
2019-10-29 17:44:20 +0100

    initial

---
+++ EmployeePersistence/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeePersistence
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Wage { get; set; }
+    }
+}
---
+++ EmployeePersistence/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmployeePersistence;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EmployeePersistance
+{
+    public class EmployeeRepository
+    {
+        private IEnumerable<Employee> employees = new List<Employee>();
+        public EmployeeRepository()
+        {
+            using (StreamReader r = new StreamReader("empList.json"))
+            {
+                string json = r.ReadToEnd();
+                employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+            }
+        }
+        public IEnumerable<Employee> Employees()
+        {
+            return employees;
+        }
+    }
+
+}
