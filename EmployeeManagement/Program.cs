65a9c059722c7c60ce65103a9787f8997e2e97a7
Piotr Myszor <PMyszor@rockwellautomation.com>
2019-10-30 09:58:16 +0100

    added print

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeePersistance;
+using EmployeePersistence;
 using System;
+using System.Collections.Generic;
 
 namespace EmployeeManagement
 {
@@ -10,11 +12,24 @@ namespace EmployeeManagement
         {
             var repo = new EmployeeRepository();
 
-            foreach (var emp in repo.Employees())
+            //foreach (var emp in repo.Employees())
+            //{
+            //    Console.WriteLine($"{emp.Id} {emp.Name}");
+            //}
+            repo.Employees().Print();
+            Console.ReadLine();
+        }
+
+    }
+    public static class Extensions
+    {
+        public static IEnumerable<Employee> Print(this IEnumerable<Employee> employees)
+        {
+            foreach (var emp in employees)
             {
                 Console.WriteLine($"{emp.Id} {emp.Name}");
             }
-            Console.ReadLine();
+            return employees;
         }
     }
 }
